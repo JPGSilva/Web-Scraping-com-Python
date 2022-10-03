22f06110a10cd2ee4432cdda9c7967290768b622
Joao Paulo G Silva <jpsilva.math@gmail.com>
2022-10-03 10:08:33 -0400

    BeautifulSoup
    Signed-off-by: Joao Paulo G Silva <jpsilva.math@gmail.com>

--- requisicoes.py
+++ requisicoes.py
@@ -1,5 +1,24 @@
+from cgitb import html
+import contextlib
+import http
+from importlib.resources import contents
+import site
 import  requests
+from bs4 import BeautifulSoup   
 
-response = requests.get('http://www.google.com/')
-print(response.status_code)
+response = requests.get('https://g1.globo.com/')
+print(response.content)
+content = response.content
+site = BeautifulSoup(content, 'html.parser')
+
+noticia = site.find('div', attrs={'class': '_evt'})
+
+#print(noticia.prettify())
+
+titulo = site.find('a', attrs={'class': 'feed-post-link'})
+print(titulo.text)
+
+
+
+# print(site.prettify())
 
